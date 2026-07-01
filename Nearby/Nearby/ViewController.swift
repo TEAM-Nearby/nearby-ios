dff1f114f86e2ca5edf5ca1617a39aeca59efcdd
sumchive <haon0319@naver.com>
2026-07-01 16:54:59 +0900

    [setting] #3 프로젝트 초기 세팅

---
+++ Nearby/Nearby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nearby
+//
+//  Created by mandoo on 7/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+

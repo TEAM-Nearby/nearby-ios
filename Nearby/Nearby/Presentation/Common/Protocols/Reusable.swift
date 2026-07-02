8de57d047af0b2b1ec4de0c6bb8c442af47e4d90
sumchive <haon0319@naver.com>
2026-07-03 03:19:44 +0900

    [setting] #5 공통 protocol 추가

---
+++ Nearby/Nearby/Presentation/Common/Protocols/Reusable.swift
@@ -0,0 +1,22 @@
+//
+//  Reusable.swift
+//  Nearby
+//
+//  Created by mandoo on 7/2/26.
+//
+
+import UIKit
+
+protocol Reusable: AnyObject {
+    static var identifier: String { get }
+}
+
+extension Reusable {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
+
+extension UICollectionReusableView: Reusable {}
+extension UITableViewCell: Reusable {}
+extension UITableViewHeaderFooterView: Reusable {}

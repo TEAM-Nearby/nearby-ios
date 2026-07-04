0fa451489eebf687d4fca508add4f6fbc6cb223a
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-04 19:58:37 +0900

    [chore] #22 네이밍 변경

--- Nearby/Nearby/Presentation/Common/Components/StarRatingInputView.swift
+++ Nearby/Nearby/Presentation/Common/Components/StarRatingInputView.swift
@@ -18,14 +18,14 @@ final class StarRatingInputView: StarRatingView {
     override func setUI() {
         super.setUI()
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapStar(_:)))
         addGestureRecognizer(tap)
     }
     
     // MARK: - Action
     
     @objc
-    private func handleTap(_ gesture: UITapGestureRecognizer) {
+    private func tapStar(_ gesture: UITapGestureRecognizer) {
         let location = gesture.location(in: self)
         let starWidth = bounds.width / CGFloat(maxRating)
         let tappedStar = Int(location.x / starWidth) + 1

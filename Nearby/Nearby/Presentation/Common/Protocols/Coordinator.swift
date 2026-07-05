121a7d52549a92d4c6a726e8af4f42a6deca0170
sumchive <haon0319@naver.com>
2026-07-06 00:58:40 +0900

    [feat] #27 Coordinator Protocol 구현

--- Nearby/Nearby/Presentation/Common/Protocols/Coordinator.swift
+++ Nearby/Nearby/Presentation/Common/Protocols/Coordinator.swift
@@ -2,11 +2,39 @@
 //  Coordinator.swift
 //  Nearby
 //
-//  Created by mandoo on 7/3/26.
+//  Created by soomin on 7/3/26.
 //
 
 import Foundation
 
 protocol Coordinator: AnyObject {
+    var parentCoordinator: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
     
+    func start()
+    func finish()
+    func addChildCoordinator(_ coordinator: Coordinator)
+    func removeChildCoordinator(_ coordinator: Coordinator)
+}
+
+extension Coordinator {
+    func start() {
+        preconditionFailure("❌ override error")
+    }
+    
+    func finish() {
+        preconditionFailure("❌ override error")
+    }
+    
+    func addChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+    
+    func removeChildCoordinator(_ coordinator: Coordinator) {
+        if let index = childCoordinators.firstIndex(where: { $0 === coordinator }) {
+            childCoordinators.remove(at: index)
+        } else {
+            print("❌ coordinator 삭제 실패: \(coordinator)")
+        }
+    }
 }

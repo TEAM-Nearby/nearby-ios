08f1657db7e679fb98dbb2bc2fa510dedd2380ec
jiinjang <jiinjang0@gmail.com>
2026-07-06 17:58:45 +0900

    [feat] #30 날짜 시간 설정 datePicker UI

--- Nearby/Nearby/Presentation/RecruitCompanion/View/RecruitCompanion.swift
+++ Nearby/Nearby/Presentation/RecruitCompanion/View/RecruitCompanion.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import SnapKit
+import Then
 
 final class RecruitCompanionView: BaseView {
     
@@ -17,26 +18,31 @@ final class RecruitCompanionView: BaseView {
     private let nowButton = NearbyButton(style: .selected, title: "지금 바로")
     private let timeButton = NearbyButton(style: .unselected, title: "시간 설정")
     private let buttonStackView = UIStackView()
+    private let datePicker = NearbyDateTimePickerView()
     
     // MARK: - Custom Methods
     
     override func setStyle() {
         backgroundColor = .white
 
-        whenTitleLabel.text = "언제 만날 예정인가요?"
-        whenTitleLabel.textColor = .grey80
-        whenTitleLabel.font = NearbyFont.b1Sb18.font
+        whenTitleLabel.do {
+            $0.text = "언제 만날 예정인가요?"
+            $0.textColor = .grey80
+            $0.font = NearbyFont.b1Sb18.font
+        }
         
-        buttonStackView.axis = .horizontal
-        buttonStackView.spacing = 12
-        buttonStackView.distribution = .fillEqually
+        buttonStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 12
+            $0.distribution = .fillEqually
+        }
+        
+        datePicker.isHidden = true
     }
 
     override func setUI() {
-        addSubview(whenTitleLabel)
-        addSubview(buttonStackView)
-        buttonStackView.addArrangedSubview(nowButton)
-        buttonStackView.addArrangedSubview(timeButton)
+        addSubviews(whenTitleLabel, buttonStackView, datePicker)
+        buttonStackView.addArrangedSubviews(nowButton, timeButton)
     }
 
     override func setLayout() {
@@ -51,28 +57,39 @@ final class RecruitCompanionView: BaseView {
             $0.horizontalEdges.equalToSuperview().inset(20)
             $0.height.equalTo(46)
         }
+
+        datePicker.snp.makeConstraints {
+            $0.top.equalTo(buttonStackView.snp.bottom).offset(18)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(154)
+        }
     }
 
     override func registerCells() {
         setAction()
     }
 
+    // MARK: - Method
+    
     private func setAction() {
         nowButton.addTarget(self, action: #selector(nowButtonDidTap), for: .touchUpInside)
         timeButton.addTarget(self, action: #selector(timeButtonDidTap), for: .touchUpInside)
     }
-
+    
     // MARK: - Action
 
     @objc
     private func nowButtonDidTap() {
         nowButton.setSelected(true)
         timeButton.setSelected(false)
+        datePicker.isHidden = true
     }
 
     @objc
     private func timeButtonDidTap() {
         nowButton.setSelected(false)
         timeButton.setSelected(true)
+        datePicker.isHidden = false
     }
+    
 }

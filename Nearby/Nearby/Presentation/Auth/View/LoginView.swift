cad60ba485d624aef470483cc03035c66d58d25f
seoyeon <hemsyeons@gmail.com>
2026-07-06 20:33:26 +0900

    [feat] #34 로그인뷰 추가

---
+++ Nearby/Nearby/Presentation/Auth/View/LoginView.swift
@@ -0,0 +1,105 @@
+//
+//  LoginView.swift
+//  Nearby
+//
+//  Created by 신서연 on 7/6/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class LoginView: BaseView {
+
+    // MARK: - UI Components
+
+    private let contentStackView = UIStackView()
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    private let logoContainerView = UIView()
+    private let logoLabel = UILabel()
+
+    let kakaoLoginButton = UIButton(type: .system)
+
+    // MARK: - Custom Methods
+
+    override func setStyle() {
+        backgroundColor = .white
+
+        contentStackView.do {
+            $0.axis = .vertical
+            $0.alignment = .center
+            $0.spacing = 12
+        }
+
+        titleLabel.do {
+            $0.textAlignment = .center
+            $0.setFont(.h1Sb24, text: "따로, 또 함께하는 여행", textColor: UIColor(red: 48/255, green: 48/255, blue: 48/255, alpha: 1))
+        }
+
+        subtitleLabel.do {
+            $0.textAlignment = .center
+            $0.setFont(.b3M14, text: "지금 로그인하고 Nearby를 시작해보세요!", textColor: UIColor(red: 126/255, green: 126/255, blue: 126/255, alpha: 1))
+        }
+
+        logoContainerView.do {
+            $0.backgroundColor = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
+        }
+
+        logoLabel.do {
+            $0.textAlignment = .center
+            $0.setFont(.b3R14, text: "로고", textColor: .black)
+        }
+
+        kakaoLoginButton.do {
+            $0.backgroundColor = UIColor(red: 1, green: 221/255, blue: 0, alpha: 1)
+            $0.layer.cornerRadius = 34
+            $0.clipsToBounds = true
+            $0.tintColor = .black
+
+            let image = UIImage(named: "icon_kakao")?.withRenderingMode(.alwaysOriginal)
+            $0.setImage(image, for: .normal)
+            $0.imageView?.contentMode = .scaleAspectFit
+        }
+    }
+
+    override func setUI() {
+        addSubviews(
+            contentStackView,
+            kakaoLoginButton
+        )
+
+        contentStackView.addArrangedSubviews(
+            titleLabel,
+            subtitleLabel,
+            logoContainerView
+        )
+
+        logoContainerView.addSubview(logoLabel)
+    }
+
+    override func setLayout() {
+        contentStackView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(253)
+            $0.centerX.equalToSuperview()
+        }
+
+        logoContainerView.snp.makeConstraints {
+            $0.width.equalTo(180)
+            $0.height.equalTo(64)
+        }
+
+        contentStackView.setCustomSpacing(36, after: subtitleLabel)
+
+        logoLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+
+        kakaoLoginButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(120)
+            $0.size.equalTo(68)
+        }
+    }
+}

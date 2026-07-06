1a2883c00931b059aaec580f48886f0f26a15c99
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-06 21:35:44 +0900

    [chore] #36 코드래빗 리뷰 반영

--- Nearby/Nearby/Presentation/Common/Components/StarRatingView.swift
+++ Nearby/Nearby/Presentation/Common/Components/StarRatingView.swift
@@ -66,10 +66,6 @@ class StarRatingView: BaseView {
     
     func rating(at point: CGPoint) -> Int {
         let convertedPoint = convert(point, to: stackView)
-        var count = 0
-        for imageView in starImageViews where convertedPoint.x >= imageView.frame.minX {
-            count += 1
-        }
-        return count
+        return starImageViews.filter { convertedPoint.x >= $0.frame.minX }.count
     }
 }

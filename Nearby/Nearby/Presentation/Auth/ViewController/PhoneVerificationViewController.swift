6ad3f0109dc379dd2b0ee771013e5a204f45dc99
seoyeon <hemsyeons@gmail.com>
2026-07-06 22:10:06 +0900

    [feat] #34 휴대폰인증 뷰컨트롤러 추가

---
+++ Nearby/Nearby/Presentation/Auth/ViewController/PhoneVerificationViewController.swift
@@ -0,0 +1,88 @@
+//
+//  PhoneVerificationViewController.swift
+//  Nearby
+//
+//  Created by 신서연 on 7/6/26.
+//
+
+import UIKit
+
+final class PhoneVerificationViewController: BaseViewController<PhoneVerificationViewModel> {
+
+    // MARK: - UI Components
+
+    private let phoneVerificationView = PhoneVerificationView()
+
+    // MARK: - Properties
+
+    private var isVerificationMode = false
+
+    // MARK: - Life Cycles
+
+    override func loadView() {
+        self.view = phoneVerificationView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setAddTarget()
+    }
+
+    // MARK: - Custom Methods
+
+    private func setAddTarget() {
+        phoneVerificationView.backButton.addTarget(
+            self,
+            action: #selector(backButtonDidTap),
+            for: .touchUpInside
+        )
+
+        phoneVerificationView.bottomButton.addTarget(
+            self,
+            action: #selector(bottomButtonDidTap),
+            for: .touchUpInside
+        )
+
+        phoneVerificationView.phoneClearButton.addTarget(
+            self,
+            action: #selector(phoneClearButtonDidTap),
+            for: .touchUpInside
+        )
+
+        phoneVerificationView.verificationClearButton.addTarget(
+            self,
+            action: #selector(verificationClearButtonDidTap),
+            for: .touchUpInside
+        )
+    }
+
+    // MARK: - Actions
+
+    @objc
+    private func backButtonDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc
+    private func bottomButtonDidTap() {
+        if isVerificationMode {
+            viewModel.action(.nextButtonDidTap)
+            print("다음 버튼 탭")
+        } else {
+            isVerificationMode = true
+            phoneVerificationView.updateVerificationMode()
+            viewModel.action(.sendVerificationButtonDidTap)
+            print("인증문자 발송하기 버튼 탭")
+        }
+    }
+
+    @objc
+    private func phoneClearButtonDidTap() {
+        phoneVerificationView.clearPhoneText()
+    }
+
+    @objc
+    private func verificationClearButtonDidTap() {
+        phoneVerificationView.clearVerificationText()
+    }
+}

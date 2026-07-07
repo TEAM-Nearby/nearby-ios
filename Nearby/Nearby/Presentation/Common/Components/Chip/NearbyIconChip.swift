523863c2e6e5e8ecf97bfa82047994326f192538
sumchive <haon0319@naver.com>
2026-07-07 15:41:12 +0900

    [feat] #33 카테고리 칩 컴포넌트 구현

---
+++ Nearby/Nearby/Presentation/Common/Components/Chip/NearbyIconChip.swift
@@ -0,0 +1,128 @@
+//
+//  NearbyIconChip.swift
+//  Nearby
+//
+//  Created by soomin on 7/6/26.
+//
+
+import SnapKit
+import UIKit
+
+final class NearbyIconChip: UIButton {
+    
+    // MARK: - Properties
+    
+    private let style: NearbyChipStyle
+    private let title: String
+    private let icon: UIImage
+    private let spacing: CGFloat = 0
+    private let iconColor: UIColor
+    
+    // MARK: - UI Components
+    
+    private let contentStackView = UIStackView()
+    private let chipIconImageView = UIImageView()
+    private let chipTextLabel = UILabel()
+    
+    // MARK: - Initializer
+    
+    init(style: NearbyChipStyle, title: String, icon: UIImage, iconColor: UIColor) {
+        self.style = style
+        self.title = title
+        self.icon = icon
+        self.iconColor = iconColor
+        super.init(frame: .zero)
+        
+        isSelected = style.isSelected
+        setStyle()
+        setUI()
+        setLayout()
+        setAddTarget()
+        setChipStyle(style)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let shadowRect = bounds.offsetBy(dx: 0, dy: style.shadowOffset.height / 2)
+        layer.shadowPath = UIBezierPath(roundedRect: shadowRect, cornerRadius: style.layerCornerRadius).cgPath
+    }
+    
+    // MARK: - Methods
+    
+    private func setStyle() {
+        clipsToBounds = false
+        layer.borderWidth = 1
+        layer.cornerRadius = style.layerCornerRadius
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = style.shadowOffset
+        layer.shadowRadius = style.shadowRadius
+        layer.shadowOpacity = style.shadowOpacity
+        isUserInteractionEnabled = style.isSelectable
+    }
+    
+    private func setUI() {
+        addSubview(contentStackView)
+        contentStackView.addArrangedSubviews(chipIconImageView, chipTextLabel)
+        
+        contentStackView.axis = .horizontal
+        contentStackView.alignment = .center
+        contentStackView.spacing = spacing
+        contentStackView.isUserInteractionEnabled = false
+        
+        chipIconImageView.image = icon.withRenderingMode(.alwaysTemplate)
+        chipIconImageView.contentMode = .scaleAspectFit
+        
+        chipTextLabel.text = title
+        chipTextLabel.textAlignment = .center
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(32)
+        }
+        
+        contentStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.leading.equalToSuperview().inset(12)
+            $0.trailing.equalToSuperview().inset(12)
+        }
+        
+        chipIconImageView.snp.makeConstraints {
+            $0.size.equalTo(24)
+        }
+    }
+    
+    private func setAddTarget() {
+        addTarget(self, action: #selector(chipDidTap), for: .touchUpInside)
+    }
+    
+    private func setChipStyle(_ style: NearbyChipStyle) {
+        backgroundColor = style.backgroundColor
+        layer.borderColor = style.borderColor.cgColor
+        chipIconImageView.tintColor = iconColor
+        chipTextLabel.textColor = style.titleColor
+        chipTextLabel.font = style.font
+        invalidateIntrinsicContentSize()
+    }
+    
+    private func updateChipButton() {
+        let toggleChipButton: NearbyChipStyle = isSelected ? style.selectedStyle : style.unselectedStyle
+        setChipStyle(toggleChipButton)
+    }
+    
+    // MARK: - Action
+    
+    @objc
+    private func chipDidTap() {
+        guard style.isSelectable else { return }
+        isSelected = !isSelected
+        updateChipButton()
+    }
+}

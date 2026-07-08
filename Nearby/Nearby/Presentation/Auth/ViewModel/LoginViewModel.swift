997d2fd1754f7765afedca315859e9974e269b8e
seoyeon <hemsyeons@gmail.com>
2026-07-08 22:38:58 +0900

    [feat] #52 로그인 뷰모델에 프라이빗 메서드 추가

--- Nearby/Nearby/Presentation/Auth/ViewModel/LoginViewModel.swift
+++ Nearby/Nearby/Presentation/Auth/ViewModel/LoginViewModel.swift
@@ -8,33 +8,60 @@
 import Foundation
 
 final class LoginViewModel: BaseViewModelType {
-
+    
     // MARK: - Input
-
+    
     enum Input {
         case kakaoLoginButtonDidTap
     }
-
+    
     // MARK: - Output
-
-    struct Output { }
-
+    
+    struct Output {
+        var loginDidSucceed: ((OnboardingStatus) -> Void)?
+        var loginDidFail: ((Error) -> Void)?
+    }
+    
     // MARK: - Property
-
-    let output: Output
-
+    
+    var output: Output
+    
+    private let kakaoAuthService = KakaoAuthService()
+    
     // MARK: - Initializer
-
+    
     init() {
         self.output = Output()
     }
-
+    
     // MARK: - Action
-
+    
     func action(_ trigger: Input) {
         switch trigger {
         case .kakaoLoginButtonDidTap:
-            break
+            loginWithKakaoAccount()
+        }
+    }
+}
+
+// MARK: - Private Method
+
+private extension LoginViewModel {
+    
+    func loginWithKakaoAccount() {
+        kakaoAuthService.loginWithKakaoAccount { [weak self] result in
+            switch result {
+            case .success(let loginData):
+                print("Nearby Access Token:", loginData.accessToken)
+                print("Nearby Refresh Token:", loginData.refreshToken)
+                print("Nearby User ID:", loginData.userId)
+                print("Onboarding Status:", loginData.onboardingStatus.rawValue)
+                
+                self?.output.loginDidSucceed?(loginData.onboardingStatus)
+                
+            case .failure(let error):
+                self?.output.loginDidFail?(error)
+            }
         }
     }
 }

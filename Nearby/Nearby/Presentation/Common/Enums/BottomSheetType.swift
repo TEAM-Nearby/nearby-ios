d5b885b9deaa9e87a077130b7ab60a86df2a4f64
sumchive <haon0319@naver.com>
2026-07-08 15:03:30 +0900

    [feat] #43 공통 바텀시트 초안 구현

--- Nearby/Nearby/Presentation/Common/Enums/BottomSheetType.swift
+++ Nearby/Nearby/Presentation/Common/Enums/BottomSheetType.swift
@@ -13,31 +13,62 @@ enum BottomSheetType {
     case nearbyCompanionBig
     case companionEmpty
     case specificCompanion
+    case specificCompanionBig
     case diningMap
-    
-    var isNearbyCompanion: Bool {
+    case diningMapFullScreen
+
+    var snapTypes: [BottomSheetType] {
         switch self {
         case .nearbyCompanionSmall, .nearbyCompanionMedium, .nearbyCompanionBig:
-            return true
-        case .companionEmpty, .specificCompanion, .diningMap:
-            return false
+            return [.nearbyCompanionSmall, .nearbyCompanionMedium, .nearbyCompanionBig]
+        case .specificCompanion, .specificCompanionBig:
+            return [.specificCompanion, .specificCompanionBig]
+        case .diningMap, .diningMapFullScreen:
+            return [.diningMap, .diningMapFullScreen]
+        case .companionEmpty:
+            return []
         }
     }
-    
-    var height: CGFloat {
+
+    var isDraggable: Bool {
+        !snapTypes.isEmpty
+    }
+
+    var smallType: BottomSheetType {
+        switch self {
+        case .nearbyCompanionSmall, .nearbyCompanionMedium, .nearbyCompanionBig:
+            return .nearbyCompanionSmall
+        case .specificCompanion, .specificCompanionBig:
+            return .specificCompanion
+        case .diningMap, .diningMapFullScreen:
+            return .diningMap
+        case .companionEmpty:
+            return .companionEmpty
+        }
+    }
+
+    var isSmallType: Bool {
+        self == smallType
+    }
+
+    var isThirdStep: Bool {
+        snapTypes.count == 3 && self == snapTypes.last
+    }
+
+    var fixedHeight: CGFloat? {
         switch self {
         case .nearbyCompanionSmall:
             return 122
         case .nearbyCompanionMedium:
             return 383
-        case .nearbyCompanionBig:
-            return 667
         case .companionEmpty:
             return 297
         case .specificCompanion:
             return 517
         case .diningMap:
             return 423
+        case .nearbyCompanionBig, .specificCompanionBig, .diningMapFullScreen:
+            return nil
         }
     }
 }

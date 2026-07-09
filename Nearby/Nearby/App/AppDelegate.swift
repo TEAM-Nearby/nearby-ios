c4034b1715b0314ea71d11d3fad77f2eb289b401
seoyeon <hemsyeons@gmail.com>
2026-07-09 15:03:16 +0900

    [feat] #60 AppDelegate에 카카오 소셜로그인 에러 캐치문 추가

--- Nearby/Nearby/App/AppDelegate.swift
+++ Nearby/Nearby/App/AppDelegate.swift
@@ -12,8 +12,8 @@ import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -24,6 +24,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             AppLogger.error(error)
             fatalError("Google Maps API key is missing. Set GOOGLE_MAPS_API_KEY before creating GMSMapView.")
         }
+        
+        do {
+            let kakaoAPIKey = try AppConfig.kakaoAPIKey()
+            KakaoSDK.initSDK(appKey: kakaoAPIKey)
+        } catch {
+            AppLogger.error(error)
+            fatalError("Kakao App key is missing. Set KAKAO_APP_KEY before initializing KakaoSDK.")
+        }
+        
         return true
     }
 
@@ -36,10 +45,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }

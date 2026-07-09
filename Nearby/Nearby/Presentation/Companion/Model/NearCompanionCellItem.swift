131c934561c022c4df2f846eb1aca799d10f4302
sumchive <haon0319@naver.com>
2026-07-10 01:48:38 +0900

    [feat] #50 주변 동행 cell 구현

---
+++ Nearby/Nearby/Presentation/Companion/Model/NearCompanionCellItem.swift
@@ -0,0 +1,18 @@
+//
+//  NearCompanionCellItem.swift
+//  Nearby
+//
+//  Created by soomin on 7/9/26.
+//
+
+import UIKit
+
+struct NearCompanionCellItem {
+    let placeImage: UIImage?
+    let placeName: String
+    let writtenTime: String
+    let content: String
+    let schedule: String
+    let participantImages: [UIImage?]
+    let statusText: String
+}

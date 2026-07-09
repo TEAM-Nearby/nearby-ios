ae1ee118597cbb2fe43b96673f810d6509f31e00
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-09 14:53:42 +0900

    [feat] #62 신고하기 뷰 생성

---
+++ Nearby/Nearby/Presentation/Report/View/Cell/ReportReasonCell.swift
@@ -0,0 +1,19 @@
+//
+//  ReportReasonCell.swift
+//  Nearby
+//
+//  Created by h2e on 7/9/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ReportReasonCell: UITableViewCell {
+    
+    // MARK: - UI Components
+    
+    private let checkbox = NearbyCheckBox(text: <#String#>)
+    
+}
---
+++ Nearby/Nearby/Presentation/Report/View/ReportPostView.swift
@@ -0,0 +1,24 @@
+//
+//  ReportPostView.swift
+//  Nearby
+//
+//  Created by h2e on 7/9/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ReportPostView: BaseView {
+    
+    // MARK: - UI Components
+    
+    private let navigationBar = NearbyNavigationBar()
+    
+    private let labelStackView = UIStackView()
+    private let titleLabel = UILabel()
+    private let subtitleLabel = UILabel()
+    
+    private let
+}

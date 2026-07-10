54acaa985a9d82f64510b200e3f73bb236f42f2f
seoyeon <hemsyeons@gmail.com>
2026-07-10 23:34:32 +0900

    [chore] #77 카카오 로그인서비스에 nonce값 로그 추가

--- Nearby/Nearby/Data/Network/Auth/Service/KakaoLoginService.swift
+++ Nearby/Nearby/Data/Network/Auth/Service/KakaoLoginService.swift
@@ -51,6 +51,8 @@ final class KakaoAuthService {
         completion: @escaping (Result<KakaoLoginDataDTO, Error>) -> Void
     ) {
         let nonce = UUID().uuidString
+        
+        // print("nonce: \(nonce)")
 
         UserApi.shared.loginWithKakaoAccount(nonce: nonce) { [weak self] oauthToken, error in
             if let error {

72032dc2344b3ae2fca3642e2012e44e495f9942
sumchive <haon0319@naver.com>
2026-07-10 18:04:50 +0900

    [feat] #67 동행 상세 ViewController 구현

---
+++ Nearby/Nearby/Presentation/Companion/ViewController/CompanionDetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CompanionDetailViewController.swift
+//  Nearby
+//
+//  Created by soomin on 7/10/26.
+//
+
+import Combine
+import UIKit
+
+final class CompanionDetailViewController: BaseViewController<CompanionDetailViewModel> {
+
+    // MARK: - UI Components
+
+    private let companionDetailView = CompanionDetailView()
+
+    // MARK: - Life Cycle
+
+    override func loadView() {
+        view = companionDetailView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    // MARK: - Custom Methods
+
+    override func setStyle() {
+        view.backgroundColor = .bgDefaultGrey
+    }
+
+    override func setAddTarget() {
+        companionDetailView.onBackButtonDidTap = { [weak self] in
+            self?.viewModel.action(.backButtonDidTap)
+        }
+
+        companionDetailView.onApplyButtonDidTap = { [weak self] in
+            self?.viewModel.action(.applyButtonDidTap)
+        }
+    }
+
+    override func bindState() {
+        viewModel.output.displayState
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] state in
+                self?.companionDetailView.configure(state: state)
+            }
+            .store(in: &cancellables)
+
+        viewModel.action(.viewDidLoad)
+    }
+}

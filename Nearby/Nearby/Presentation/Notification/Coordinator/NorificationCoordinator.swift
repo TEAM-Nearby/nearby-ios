7e94ec773866fa067902a4f31dfbd1c24a72cb28
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-11 23:13:14 +0900

    [feat] #85 디자인 일부 수정

---
+++ Nearby/Nearby/Presentation/Notification/Coordinator/NorificationCoordinator.swift
@@ -0,0 +1,44 @@
+//
+//  NorificationCoordinator.swift
+//  Nearby
+//
+//  Created by h2e on 7/11/26.
+//
+
+import UIKit
+
+final class NotificationCoordinator {
+    
+    // MARK: - Properties
+    
+    weak var parentCoordinator: Coordinator?
+    var childCoordinators = [Coordinator]()
+    private let navigationController: UINavigationController
+    private let diContainer: AppDIContainer
+    
+    private weak var reportReturnViewController: UIViewController?
+    
+    // MARK: - Initializer
+    
+    init(navigationController: UINavigationController, diContainer: AppDIContainer) {
+        self.navigationController = navigationController
+        self.diContainer = diContainer
+    }
+}
+
+// MARK: - Coordinator
+
+extension NotificationCoordinator: Coordinator {
+    func start() {
+//        let viewController = diContainer.makeAlarmViewController()(coordinator: self)
+//        navigationController.setViewControllers([viewController], animated: false)
+    }
+    
+    func finish() {
+        parentCoordinator?.removeChildCoordinator(self)
+    }
+    
+    func showCompanionTab() {
+        (parentCoordinator as? MainTabCoordinator)?.switchTab(to: .companion)
+    }
+}

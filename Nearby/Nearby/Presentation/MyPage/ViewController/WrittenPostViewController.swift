018d42e72cfc1acdf61679aa533fcb5f46a95408
seoyeon <hemsyeons@gmail.com>
2026-07-11 22:21:48 +0900

    [feat] #82 작성한 모집글 뷰컨트롤러 추가

---
+++ Nearby/Nearby/Presentation/MyPage/ViewController/WrittenPostViewController.swift
@@ -0,0 +1,108 @@
+//
+//  WrittenPostViewController.swift
+//  Nearby
+//
+//  Created by 신서연 on 7/11/26.
+//
+
+import UIKit
+
+final class WrittenPostViewController:
+    BaseViewController<WrittenPostViewModel> {
+
+    // MARK: - Properties
+
+    var onBackButtonDidTap: (() -> Void)?
+    var onFindCompanionButtonDidTap: (() -> Void)?
+
+    private var writtenPostItems: [WrittenPostItem] = []
+
+    // MARK: - UI Component
+
+    private let writtenPostView = WrittenPostView()
+
+    // MARK: - Life Cycles
+
+    override func loadView() {view = writtenPostView}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.action(.viewDidLoad)
+    }
+
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    // MARK: - Custom Methods
+
+    override func setAddTarget() {
+        writtenPostView.navigationBar.leftButtonAction = { [weak self] in
+            self?.viewModel.action(.backButtonDidTap)
+        }
+
+        writtenPostView.findCompanionButton.addTarget(
+            self, action: #selector(findCompanionButtonDidTap),
+            for: .touchUpInside
+        )
+    }
+
+    override func setDelegate() {
+        writtenPostView.tableView.dataSource = self
+        writtenPostView.tableView.delegate = self
+    }
+
+    override func bindState() {
+        viewModel.output.writtenPostItems = { [weak self] items in
+
+            guard let self else { return }
+
+            writtenPostItems = items
+            writtenPostView.tableView.reloadData()
+            writtenPostView.updateContent(items: items)
+        }
+
+        viewModel.output.backButtonDidTap = { [weak self] in
+            self?.onBackButtonDidTap?()
+        }
+
+        viewModel.output.findCompanionButtonDidTap = { [weak self] in
+            self?.onFindCompanionButtonDidTap?()
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension WrittenPostViewController:
+    UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return writtenPostItems.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+
+        cell.selectionStyle = .none
+        cell.backgroundColor = .white
+
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension WrittenPostViewController:
+    UITableViewDelegate {}
+
+// MARK: - Action
+
+private extension WrittenPostViewController {
+    @objc
+    func findCompanionButtonDidTap() {viewModel.action(.findCompanionButtonDidTap)}
+}

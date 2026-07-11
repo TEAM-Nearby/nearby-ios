439f7de6e8ad440a9ccb81f6c9ea0f83dbf59f11
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-12 00:29:49 +0900

    [feat] #85 별점 스와이프 액션 추가

--- Nearby/Nearby/Presentation/Common/Components/StarRating/StarRatingInputView.swift
+++ Nearby/Nearby/Presentation/Common/Components/StarRating/StarRatingInputView.swift
@@ -20,9 +20,20 @@ final class StarRatingInputView: StarRatingView {
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(tapStar(_:)))
         addGestureRecognizer(tap)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(panStar(_:)))
+        addGestureRecognizer(pan)
     }
     
-    // MARK: - Action
+    // MARK: - Life Cycle
+    
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let pan = gestureRecognizer as? UIPanGestureRecognizer else { return true }
+        let velocity = pan.velocity(in: self)
+        return abs(velocity.x) > abs(velocity.y)
+    }
+    
+    // MARK: - Actions
     
     @objc
     private func tapStar(_ gesture: UITapGestureRecognizer) {
@@ -30,6 +41,20 @@ final class StarRatingInputView: StarRatingView {
         let tappedStar = rating(at: location)
         
         setRating(tappedStar)
+        onRatingChanged?(rating)
+    }
+    
+    @objc
+    private func panStar(_ gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: self)
+        updateRating(to: rating(at: location))
+    }
+    
+    // MARK: - Method
+    
+    private func updateRating(to newRating: Int) {
+        guard newRating != rating else { return }
+        setRating(newRating)
         onRatingChanged?(rating)
     }
 }

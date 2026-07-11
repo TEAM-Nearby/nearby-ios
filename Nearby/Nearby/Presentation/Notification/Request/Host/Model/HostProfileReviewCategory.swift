e8a2b47aca176f61ea50580c242bcc37c2f8819e
seoyeon <hemsyeons@gmail.com>
2026-07-12 06:40:42 +0900

    [feat] #83 호스트 프로필 리뷰 카테고리 추가

---
+++ Nearby/Nearby/Presentation/Notification/Request/Host/Model/HostProfileReviewCategory.swift
@@ -0,0 +1,13 @@
+//
+//  HostProfileReviewCategory.swift
+//  Nearby
+//
+//  Created by 신서연 on 7/12/26.
+//
+
+import Foundation
+
+enum HostProfileReviewCategory {
+    case communication
+    case punctuality
+}

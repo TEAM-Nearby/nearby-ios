6af098b86bd44e9f370dde9bf7b431086d020d3c
sumchive <haon0319@naver.com>
2026-07-11 22:33:22 +0900

    [chore] #79 혼밥지도 바텀시트 resolver 추가

--- Nearby/Nearby/Presentation/Common/Components/BottomSheet/NearbyBottomSheetHeightResolver.swift
+++ Nearby/Nearby/Presentation/Common/Components/BottomSheet/NearbyBottomSheetHeightResolver.swift
@@ -31,7 +31,11 @@ enum NearbyBottomSheetHeightResolver {
             return NearbyBottomSheetValue.nearCompanionCompactHeight + context.tabBarHeight
         case .specificRestaurantCompanionList:
             return NearbyBottomSheetValue.specificCompanionCompactHeight
-        case .nearbyCompanionEmpty, .diningMapList, .savedRestaurantList:
+        case .diningMapList:
+            return NearbyBottomSheetValue.diningListCompactHeight + context.tabBarHeight
+        case .savedRestaurantList:
+            return NearbyBottomSheetValue.savedDiningListCompactHeight
+        case .nearbyCompanionEmpty:
             return NearbyBottomSheetValue.nearbyCompanionEmptyCompactHeight
         }
     }
@@ -44,8 +48,10 @@ enum NearbyBottomSheetHeightResolver {
             return companionListStandardHeight(context: context) + NearbyBottomSheetValue.companionListStandardHeightAddition
         case .nearbyCompanionList:
             return max(0, companionListStandardHeight(context: context) + NearbyBottomSheetValue.companionListStandardHeightAddition)
-        case .diningMapList, .savedRestaurantList:
+        case .diningMapList:
             return NearbyBottomSheetValue.diningListStandardHeight
+        case .savedRestaurantList:
+            return NearbyBottomSheetValue.savedDiningListStandardHeight
         }
     }
     

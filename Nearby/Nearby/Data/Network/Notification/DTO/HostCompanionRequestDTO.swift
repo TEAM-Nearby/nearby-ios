2701960c5090f6feb4a3d1f8746e9078612c2909
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-12 22:18:07 +0900

    [network] #100 동행 신청 및 수락 DTO 작성

---
+++ Nearby/Nearby/Data/Network/Notification/DTO/HostCompanionRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  HostCompanionRequestDTO.swift
+//  Nearby
+//
+//  Created by h2e on 7/12/26.
+//
+
+import Foundation
+
+struct HostCompanionDeclineRequestDTO {
+    let rejectionReason: String?
+}

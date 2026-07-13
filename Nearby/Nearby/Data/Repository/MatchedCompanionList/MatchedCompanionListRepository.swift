282d8e07bffc5774aaf775e7453180138d7a0ef3
jiinjang <jiinjang0@gmail.com>
2026-07-14 02:12:59 +0900

    [feat] #136 매칭된 동행 미리보기 Repository 정의

--- Nearby/Nearby/Data/Repository/MatchedCompanionList/MatchedCompanionListRepository.swift
+++ Nearby/Nearby/Data/Repository/MatchedCompanionList/MatchedCompanionListRepository.swift
@@ -7,7 +7,7 @@
 
 protocol MatchedCompanionListRepository {
     func fetchMatches() async throws -> MatchedCompanionListResponseDTO
-    func fetchMatchDetail(matchId: Int) async throws -> MatchingScheduleDetailResponseModel
+    func fetchMatchPreview(matchId: Int) async throws -> MatchedCompanionPreviewResponseDTO
 }
 
 final class DefaultMatchedCompanionListRepository {
@@ -30,7 +30,7 @@ extension DefaultMatchedCompanionListRepository: MatchedCompanionListRepository 
         try await service.fetchMatches()
     }
 
-    func fetchMatchDetail(matchId: Int) async throws -> MatchingScheduleDetailResponseModel {
-        try await service.fetchMatchDetail(matchId: matchId)
+    func fetchMatchPreview(matchId: Int) async throws -> MatchedCompanionPreviewResponseDTO {
+        try await service.fetchMatchPreview(matchId: matchId)
     }
 }

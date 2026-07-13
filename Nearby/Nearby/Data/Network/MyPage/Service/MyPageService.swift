60b4e1fc0aa33e44c27c367545c64e6386005d06
seoyeon <hemsyeons@gmail.com>
2026-07-14 03:21:32 +0900

    [feat] #139 마이페이지 서비스 추가

---
+++ Nearby/Nearby/Data/Network/MyPage/Service/MyPageService.swift
@@ -0,0 +1,35 @@
+//
+//  MyPageService.swift
+//  Nearby
+//
+//  Created by 신서연 on 7/14/26.
+//
+
+protocol MyPageService {
+    func fetchMyPage() async throws -> MyPageResponseDTO
+}
+
+final class DefaultMyPageService {
+
+    // MARK: - Properties
+
+    private let networkProvider: NetworkProvider
+
+    // MARK: - Initializer
+
+    init(networkProvider: NetworkProvider) {
+        self.networkProvider = networkProvider
+    }
+}
+
+// MARK: - MyPageService
+
+extension DefaultMyPageService: MyPageService {
+
+    func fetchMyPage() async throws -> MyPageResponseDTO {
+        try await networkProvider.request(
+            MyPageTarget.fetchMyPage,
+            responseType: MyPageResponseDTO.self
+        )
+    }
+}

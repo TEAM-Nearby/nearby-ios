07f5fdeb5b66eb5527f76c2bbf9bad1f733da973
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-14 04:18:23 +0900

    [network] #125 동행 마치기 Repository 정의

--- Nearby/Nearby/Data/Repository/Review/ReviewRepository.swift
+++ Nearby/Nearby/Data/Repository/Review/ReviewRepository.swift
@@ -8,6 +8,7 @@
 protocol ReviewRepository {
     func createReview(meetingId: Int, request: CreateReviewRequestDTO) async throws -> CreateReviewResponseDTO
     func fetchReviewTargets(meetingId: Int) async throws -> ReviewTargetsResponseDTO
+    func completeMeeting(meetingId: Int) async throws -> ReviewCompleteDTO
 }
 
 final class DefaultReviewRepository: ReviewRepository {
@@ -23,5 +24,9 @@ final class DefaultReviewRepository: ReviewRepository {
 
     func fetchReviewTargets(meetingId: Int) async throws -> ReviewTargetsResponseDTO {
         try await service.fetchReviewTargets(meetingId: meetingId)
+    }
+    
+    func completeMeeting(meetingId: Int) async throws -> ReviewCompleteDTO {
+        try await service.completeMeeting(meetingId: meetingId)
     }
 }

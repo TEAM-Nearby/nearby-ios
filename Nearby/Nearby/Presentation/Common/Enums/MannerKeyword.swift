893fcac47a930e2df89bbd2f4f7debc470ab4421
sumchive <haon0319@naver.com>
2026-07-17 23:09:18 +0900

    [fix] #203 작성자 동행 후기 키워드 매핑

--- Nearby/Nearby/Presentation/Common/Enums/MannerKeyword.swift
+++ Nearby/Nearby/Presentation/Common/Enums/MannerKeyword.swift
@@ -27,7 +27,7 @@ enum MannerKeyword: String {
     var title: String {
         switch self {
         case .fastResponse:
-            return "연락이 빨라요"
+            return "답장이 빨라요"
 
         case .goodManners:
             return "매너가 좋아요"
@@ -36,19 +36,19 @@ enum MannerKeyword: String {
             return "대화가 잘 통해요"
 
         case .goodTalker:
-            return "입담이 좋아요"
+            return "대화를 잘 이끌어요"
 
         case .informative:
-            return "유용한 정보를 많이 알아요"
+            return "정보를 잘 알려줘요"
 
         case .punctual:
-            return "시간 약속을 잘 지켜요"
+            return "약속 시간을 잘 지켜요"
 
         case .notifyDelayInAdvance:
-            return "늦어도 미리 알려줘요"
+            return "늦을 때 미리 알려줘요"
 
         case .arrivesEarly:
-            return "약속 시간보다 일찍 와요"
+            return "약속 장소에 일찍 도착해요"
         }
     }
 

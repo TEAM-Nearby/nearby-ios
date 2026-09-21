84424e6d13ce3b27d267935754130ce3267a1419
seoyeon <hemsyeons@gmail.com>
2026-09-22 01:08:46 +0900

    [chore] #220 린트 컨벤션 맞춰서 공백 제거

--- Nearby/Nearby/Presentation/MyPage/ViewController/WrittenPostViewController.swift
+++ Nearby/Nearby/Presentation/MyPage/ViewController/WrittenPostViewController.swift
@@ -117,4 +117,3 @@ extension WrittenPostViewController: UITableViewDataSource {
 
 extension WrittenPostViewController: UITableViewDelegate { func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { tableView.deselectRow(at: indexPath, animated: false) } }
 
-
